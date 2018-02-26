77a1b21431a45674ae88f53a6e1aa4f3f7e9deee
anitha-raveen <anitha.raveendran@gmail.com>
2018-02-26 00:37:15 -0500

    added concurrent list example

---
+++ src/main/java/com/learn/collections/CopyOnWriteArrayListExample.java
@@ -0,0 +1,38 @@
+package com.learn.collections;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CopyOnWriteArrayListExample {
+
+	public static void main(String[] args) {
+		List<String> myList = new CopyOnWriteArrayList<String>();
+		
+		myList.add("1");
+		myList.add("2");
+		myList.add("3");
+		myList.add("4");
+		myList.add("5");
+		myList.forEach(System.out::println);
+		Iterator<String> it = myList.iterator();
+		while(it.hasNext()){
+			String value = it.next();
+			System.out.println("List Value:"+value);
+			System.out.println("Item @ index 3 " + myList.get(3));
+			if(value.equals("3")){
+				myList.remove("4");
+				//myList.forEach(System.out::println);
+				myList.add("6");
+				myList.add("7");
+				//myList.forEach(System.out::println);
+				System.out.println("Item @ index 3 " + myList.get(3));
+				
+			}
+		}
+		System.out.println("List Size:"+myList.size());
+		
+		myList.stream().forEach(System.out::println);
+	}
+
+}
